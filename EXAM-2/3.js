3884a15a688bf49605d3545bb6b3fd5457824c13
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-07-08 22:40:40 +0530

    08-06-24

---
+++ EXAM-2/3.js
@@ -0,0 +1,15 @@
+let n = 10;
+let evensum = 0;
+let oddsum = 0;
+for (let i = 0; i < n; i++) {
+  if (i % 2 == 0) {
+    evensum += i;
+  } else {
+    oddsum += i;
+  }
+}
+if (evensum > oddsum) {
+  console.log(evensum - oddsum);
+} else {
+  console.log(oddsum - evensum);
+}
