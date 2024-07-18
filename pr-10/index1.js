a43f7887141609202203d3fb47c5650dd7e52591
Divyesh Khandare <divyeshkhandare@gmail.com>
2024-07-18 10:23:40 +0530

    Js

--- pr-10/index1.js
+++ pr-10/index1.js
@@ -1,7 +1,8 @@
+// ARRAY TO LOWERCASE at alternative postion
+
 let array = ["RED", "AND", "WHITE"];
-let n = array.length;
-for (let i = 0; i < n; i++) {
-  if (array[i] == "AND") {
+for (let i = 0; i < array.length; i++) {
+  if (i % 2 !== 0) {
     console.log(array[i].toLowerCase());
   } else {
     console.log(array[i]);
