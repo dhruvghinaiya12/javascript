ffdf45b83d6611267b9ec24a4d0aa91039b41075
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-08 14:03:14 +0530

    07-08-24

--- extra-1/index.js
+++ extra-1/index.js
@@ -81,3 +81,29 @@ const handleDelete = (index) => {
 };
 
 document.getElementById("dragonForm").addEventListener("submit", handleData);
+
+/*--------------------------------------------------------slider-----------------------------------------------------------*/ 
+  const slides = document.querySelector('.slides');
+    const slide = document.querySelectorAll('.slide');
+    const prev = document.getElementById('prev');
+    const next = document.getElementById('next');
+
+    let index = 0;
+
+    function showSlide(i) {
+        index += i;
+        if (index < 0) {
+            index = slide.length - 1;
+        }
+        if (index >= slide.length) {
+            index = 0;
+        }
+        slides.style.transform = `translateX(${-index * 100}%)`;
+    }
+
+    prev.addEventListener('click', () => showSlide(-1));
+    next.addEventListener('click', () => showSlide(1));
+
+    setInterval(() => {
+        showSlide(1);
+    }, 3000);
