177a832cc3fed5044345d98fe098f5453eb9c67d
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-14 21:09:03 +0530

    14-08-24

---
+++ pr-14/index.js
@@ -0,0 +1,44 @@
+const time = document.getElementById("time");
+const ampm = document.getElementById("ampm");
+const dateElement = document.getElementById("date");
+
+
+  setInterval(() =>{
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  if (hours < 10) {
+    hours = "0" + hours;
+} else {
+    hours = hours;
+}
+
+if (minutes < 10) {
+    minutes = "0" + minutes;
+} else {
+    minutes = minutes;
+}
+
+if (seconds < 10) {
+    seconds = "0" + seconds;
+} else {
+    seconds = seconds;
+}
+
+if (date.getHours() >= 12) {
+    ampm.innerHTML = "PM";
+} else {
+    ampm.innerHTML = "AM";
+}
+
+  time.innerHTML = `${hours}:${minutes}:${seconds}`;
+  
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const dayOfMonth = date.getDate();
+  const year = date.getFullYear();
+  const weekDay = days[date.getDay()];
+  const month = months[date.getMonth()];
+  dateElement.innerHTML = `${weekDay}, ${dayOfMonth} ${month} ${year}`;
+},1000)
