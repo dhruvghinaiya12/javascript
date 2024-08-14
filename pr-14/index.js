1bb1d5b6182dd316d5e68bc8a8eea4195acaa385
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-14 22:54:13 +0530

    14-08-24

--- pr-14/index.js
+++ pr-14/index.js
@@ -2,38 +2,36 @@ const time = document.getElementById("time");
 const ampm = document.getElementById("ampm");
 const dateElement = document.getElementById("date");
 
-
-  setInterval(() =>{
+function updateTimeAndDate() {
   const date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+
   if (hours < 10) {
     hours = "0" + hours;
-} else {
-    hours = hours;
-}
+  } 
 
-if (minutes < 10) {
+  if (minutes < 10) {
     minutes = "0" + minutes;
-} else {
-    minutes = minutes;
-}
+  } 
 
-if (seconds < 10) {
+  if (seconds < 10) {
     seconds = "0" + seconds;
-} else {
-    seconds = seconds;
-}
+  } 
 
-if (date.getHours() >= 12) {
+  if (date.getHours() >= 12) {
     ampm.innerHTML = "PM";
-} else {
+  } else {
     ampm.innerHTML = "AM";
-}
+  }
+
+  if (hours > 12) {
+    hours -= 12;
+  }
 
   time.innerHTML = `${hours}:${minutes}:${seconds}`;
-  
+
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const dayOfMonth = date.getDate();
@@ -41,4 +39,8 @@ if (date.getHours() >= 12) {
   const weekDay = days[date.getDay()];
   const month = months[date.getMonth()];
   dateElement.innerHTML = `${weekDay}, ${dayOfMonth} ${month} ${year}`;
-},1000)
+}
+
+updateTimeAndDate();
+
+setInterval(updateTimeAndDate, 1000);
