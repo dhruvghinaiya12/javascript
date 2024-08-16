ccd75588ad78e1e01164fc1e5dcf956d7108c6c9
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-16 23:03:21 +0530

    16-08-24

---
+++ pr-15/index.js
@@ -0,0 +1,53 @@
+function customer() {
+    let count = 0;
+    let customerCount = document.getElementById("customerCount").innerHTML;
+    let Customer = setInterval(() => {
+      document.getElementById("customerCount").innerHTML = count;
+      count++;
+      if (count == Number(customerCount) + 1) {
+        clearInterval(Customer);
+        client(); 
+      }
+    }, 10);
+  }
+  
+  function client() {
+    let clientCount = 0;
+    let clientTotal = document.getElementById("clientCount").innerHTML;
+    let Client = setInterval(() => {
+      document.getElementById("clientCount").innerHTML = clientCount;
+      clientCount++;
+      if (clientCount == Number(clientTotal) + 1) {
+        clearInterval(Client);
+        projects(); 
+      }
+    }, 10);
+  }
+  
+  function projects() {
+    let projectCount = 0;
+    let projectsTotal = document.getElementById("projectsCount").innerHTML;
+    let Projects = setInterval(() => {
+      document.getElementById("projectsCount").innerHTML = projectCount;
+      projectCount++;
+      if (projectCount == Number(projectsTotal) + 1) {
+        clearInterval(Projects);
+        coffeeWithClient(); 
+      }
+    }, 10);
+  }
+  
+  function coffeeWithClient() {
+    let coffeeCount = 0;
+    let coffeeTotal = document.getElementById("coffeeWithClientCount").innerHTML;
+    let Coffee = setInterval(() => {
+      document.getElementById("coffeeWithClientCount").innerHTML = coffeeCount;
+      coffeeCount++;
+      if (coffeeCount == Number(coffeeTotal) + 1) {
+        clearInterval(Coffee);
+      }
+    }, 10);
+  }  
+  customer();
+
+  
