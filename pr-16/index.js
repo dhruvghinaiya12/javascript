e5e3e9d4e99dc3525d12c61037c5fa1a6158a521
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-21 12:16:21 +0530

    20-08-24

--- pr-16/index.js
+++ pr-16/index.js
@@ -1,4 +1,5 @@
-document.getElementById('btn').addEventListener('click', function() {
-    const Dice = Math.floor(Math.random() * 6) + 1;
-    document.getElementById('Number').innerText = Dice;
-});
+function rollDice() {
+    const Dice = Math.trunc(Math.random() * 6) + 1;
+  document.getElementById("Number").innerText = Dice;
+}
+document.getElementById("btn").addEventListener("click", rollDice);
