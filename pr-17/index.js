4037eabe3c7deb5f0609696d0e02ba3b393ebe0a
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-23 17:32:22 +0530

    21-08-24

--- pr-17/index.js
+++ pr-17/index.js
@@ -2,37 +2,49 @@ let likeCount = Number(localStorage.getItem("likeCount") || 0);
 const likeCountSpan = document.getElementById("LikeCount");
 likeCountSpan.innerHTML = likeCount;
 document.getElementById("like").addEventListener("click", () => {
-  likeCount ++;
+  likeCount++;
   likeCountSpan.innerHTML = likeCount;
   localStorage.setItem("likeCount", likeCount);
 });
 
-let CommentCount = Number(localStorage.getItem("commentCount")|| 0);
+let commentCount = Number(localStorage.getItem("commentCount") || 0);
 const commentCountSpan = document.getElementById("CommentCount");
-commentCountSpan.innerHTML = CommentCount;
+commentCountSpan.innerHTML = commentCount;
 document.getElementById("comment").addEventListener("click", () => {
-  CommentCount ++;
-  commentCountSpan.innerHTML = CommentCount;
-  localStorage.setItem("commentCount", CommentCount);
+  commentCount++;
+  commentCountSpan.innerHTML = commentCount;
+  localStorage.setItem("commentCount", commentCount);
 });
 
-let shareCount = Number(sessionStorage.getItem("share")|| 0);
+let shareCount = Number(sessionStorage.getItem("shareCount") || 0);
 const shareCountSpan = document.getElementById("ShareCount");
 shareCountSpan.innerHTML = shareCount;
 
 document.getElementById("share").addEventListener("click", () => {
-  shareCount++;
-  shareCountSpan.innerHTML = shareCount;
-  sessionStorage.setItem("shareCount", shareCount);
+  if (navigator.share) {
+    navigator.share({
+      title: 'Swiss Golden Hour at Edinburgh Castle',
+      text: 'Check out this amazing post!',
+      url: window.location.href
+    }).then(() => {
+      shareCount++;
+      shareCountSpan.innerHTML = shareCount;
+      sessionStorage.setItem("shareCount", shareCount);
+    }).catch((error) => {
+      console.error('Sharing failed', error);
+    });
+  } else {
+    alert('Web Share API is not supported on your device/browser.');
+  }
 });
 
-let downloadCount = Number(sessionStorage.getItem("downloadCount")|| 0);
-const downloadCountSpan = document.getElementById("DownloadCount");
 
+let downloadCount = Number(sessionStorage.getItem("downloadCount") || 0);
+const downloadCountSpan = document.getElementById("DownloadCount");
 downloadCountSpan.innerHTML = downloadCount;
 
 document.getElementById("bookmark").addEventListener("click", () => {
-    downloadCount ++;
+  downloadCount++;
   downloadCountSpan.innerHTML = downloadCount;
   sessionStorage.setItem("downloadCount", downloadCount);
 });
