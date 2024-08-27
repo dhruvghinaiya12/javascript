15c53f637837a841836097556e32d26ea5c24707
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-08-27 23:05:20 +0530

    27-08-24

--- Exam-7/index.js
+++ Exam-7/index.js
@@ -15,18 +15,29 @@ const handledata = (user) => {
   });
 };
 
-const handleusers = (users) => {
+const handleusers = (users,index) => {
   document.getElementById("main").innerHTML = "";
-
   users.map((newsdata) => {
+    let div = document.createElement("div");
+    div.className = "news-box";
     let image = document.createElement("img");
     image.src = newsdata.image;
+    let content = document.createElement("div");
+    content.className = "news-content";
     let title = document.createElement("h2");
     title.innerHTML = newsdata.title;
     let p = document.createElement("p");
     p.innerHTML = newsdata.description;
-    let div = document.createElement("div");
-    div.append(image, title, p); 
+    let deletebtn = document.createElement("button");
+    deletebtn.innerHTML = "Delete";
+    deletebtn.className = "delete-btn";
+    deletebtn.addEventListener("click", () => {
+      users.splice(index, 1);
+      localStorage.setItem("users", JSON.stringify(users));
+      handleusers(users);
+    });
+    content.append(title, p, deletebtn);
+    div.append(image, content);
     document.getElementById("main").append(div);
   });
 };
