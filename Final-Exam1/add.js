ad9d692c3edf74a3a2c39a60b9318b69a544ad6f
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-10-02 13:10:18 +0530

    01-10-24

--- Final-Exam1/add.js
+++ Final-Exam1/add.js
@@ -1,23 +1,42 @@
 import ApiMethods, { navbar } from "./components/navbar.js";
 document.getElementById("navbar").innerHTML = navbar();
 
-
 const AddData = async (e) => {
-    e.preventDefault();
-  
-let Data={
-   question:document.getElementById("Que").value,
-   option1:document.getElementById("option-a").value,
-   option2:document.getElementById("option-b").value,
-   option3:document.getElementById("option-c").value,
-   option4:document.getElementById("option-d").value,
-   answer:document.getElementById("answer").value
-}
-await postData(Data);
+  e.preventDefault();
+
+  let Data = {
+    question: document.getElementById("Que").value,
+    option1: document.getElementById("option-a").value,
+    option2: document.getElementById("option-b").value,
+    option3: document.getElementById("option-c").value,
+    option4: document.getElementById("option-d").value,
+    answer: document.getElementById("answer").value,
+  };
+  if (
+    Data.answer !== Data.option1 &&
+    Data.answer !== Data.option2 &&
+    Data.answer !== Data.option3 &&
+    Data.answer !== Data.option4
+  ) {
+    alert("enter correct answer.");
+    return;
+  }
+  if (
+    Data.option1 === Data.option2 ||
+    Data.option1 === Data.option3 ||
+    Data.option1 === Data.option4 ||
+    Data.option2 === Data.option3 ||
+    Data.option2 === Data.option4 ||
+    Data.option3 === Data.option4
+  ) {
+    alert("Make sure all options are different.");
+    return;
+  }
+  await postData(Data);
 };
 
 document.getElementById("UserData").addEventListener("submit", AddData);
 
 const postData = async (Data) => {
-    await ApiMethods.post(Data)
-  };
+  await ApiMethods.post(Data);
+};
